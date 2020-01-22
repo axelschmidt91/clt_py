354ba8c51460e451c12a04f37e73fc0f44498cd3
axelschmidt91 <35659663+axelschmidt91@users.noreply.github.com>
2020-01-22 20:31:18 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import clt_py
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 
-              'sphinx.ext.viewcode'
+              'sphinx.ext.viewcode',
               'sphinx.ext.todo',
               'sphinx.ext.githubpages',]
 
