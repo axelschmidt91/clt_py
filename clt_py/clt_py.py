35d473216335d83a73981e89c682cd1dc08d167d
Axel Schmidt <axel.sebastian.schmidt@rwth-aachen.de>
2020-01-23 01:05:02 +0100

    add Todos, start modelling implementation

--- clt_py/clt_py.py
+++ clt_py/clt_py.py
@@ -14,7 +14,16 @@ class OverDeterminedError(Exception):
 
 class Material:
 
+    class NotAnisotropicError(Exception):
+        pass
+
+    class NotIsotropicError(Exception):
+        pass
+
+    # TODO: Subclasses for Anisitropic Material and isotropic Material. --> Independend material properties are different
+
     def __init__(self, rho, E=None, G=None, v=None):
+        super().__init__()
         self.rho = rho
         if (G is not None) and (v is not None) and (E is not None):
             raise OverDeterminedError()
@@ -46,8 +55,7 @@ class Material:
                     self.v = self.v[0]
                 else:
                     raise OverDeterminedError()
-        else:
-            raise NotImplementedError()
+
         self.isotropic = True
         if isinstance(self.E, list):
             self.isotropic = False
@@ -55,17 +63,68 @@ class Material:
 
 class Ply:
 
+    system = "simplified"  # "simplified" or "hsb"
+
     def __init__(self, matFib, matMat, fibVolRatio=0.5):
         super().__init__()
-        self.matFib = matFib
-        self.matMat = matMat
-        self.fibVolRatio = fibVolRatio
+        self.set_matFib(matFib)
+        self.set_matMat(matMat)
+        self.set_fibVolRatio(fibVolRatio)
 
     def set_fibWgRatio(self, fibWgRatio):
         self.fibVolRatio = (fibWgRatio * self.matMat.rho) / (fibWgRatio * self.matMat.rho + (1 - fibWgRatio) * self.matFib.rho)
+        self.update()
 
     def set_fibVolRatio(self, fibVolRatio):
         self.fibVolRatio = fibVolRatio
+        self.update()
+
+    def set_matFib(self, matFib):
+        self.check_matFib(matFib)
+        self.matFib = matFib
+        self.update()
+
+    def set_matMat(self, matMat):
+        self.check_matMat(matMat)
+        self.matMat = matMat
+        self.update()
+
+    def check_matFib(self, matFib):
+        # TODO: check for isinstance of Anisotropic class
+        if isinstance(matFib, list):
+            if len(matFib) <= 1:
+                raise Material.NotAnisotropicError()
+        else:
+            raise Material.NotAnisotropicError()
+
+    def check_matMat(self, matMat):
+        # TODO: check for isinstance of Isotropic class
+        if isinstance(matMat, list):
+            raise Material.NotIsotropicError()
+
+    def update(self):
+        if system is "simplified":
+            self.prismatic_jones_model()
+        elif system is "hsb":
+            self.hsb_model()
+        else:
+            raise ValueError
+
+    def prismatic_jones_model(self):
+        raise NotImplementedError()
+        E_para = self.matFib.E[0] * self.fibVolRatio + self.matMat.E * (1 - self.fibVolRatio)
+        E_orto = (self.matFib.E[1] * self.matMat.E) / (self.matMat.E * self.fibVolRatio + self.matFib.E[1] * (1 - self.fibVolRatio))
+        self.E = [E_para, E_orto]
+        self.G = (self.matFib.G) / ()
+        # self.v =
+        # self.rho =
+
+    def hsb_model(self):
+        # TODO: Implementation
+        raise NotImplementedError()
+        self.E = 0
+        self.G = 0
+        self.rho = 0
 
 
 class Laminate:
@@ -75,5 +134,3 @@ class Laminate:
 
     def addPly(self, rotation=0):
         pass
-
-    pass
