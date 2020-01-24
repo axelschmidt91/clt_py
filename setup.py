f59083a5a6f838ec32eae7e017f2b7d8d826426d
Axel Schmidt <axel.sebastian.schmidt@rwth-aachen.de>
2020-01-24 16:01:27 +0100

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/axelschmidt91/clt_py",
-    version="0.0.2",
+    version="version='0.0.3'",
     zip_safe=False,
 )
