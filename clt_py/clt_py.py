fix: rotationmatrix calculation

clt_py/clt_py.py
@@ -53,7 +53,7 @@ class Material2D:
 
 
 class IsotropicMaterial(Material2D):
-    def __init__(self, rho, label="material", E=None, G=None, v=None):
+    def __init__(self, rho, E=None, G=None, v=None, label="material"):
         if (G is not None) and (v is not None) and (E is not None):
             raise OverDeterminedError()
         elif (
@@ -273,6 +273,7 @@ class Ply:
         self.calc_rotationStressMatrix()
         self.calc_stiffnessMatrix()
         self.calc_complianceMatrix()
+        self.calc_engineer_constantes()
 
     def check_materialType(self, material):
         if isinstance(material, FiberReinforcedMaterialUD):
@@ -288,19 +289,19 @@ class Ply:
         self.rotStress = np.array(
             [
                 [
-                    sqrt(cos(self.rotRad)),
-                    sqrt(sin(self.rotRad)),
+                    (cos(self.rotRad)) ** 2,
+                    (sin(self.rotRad)) ** 2,
                     2 * sin(self.rotRad) * cos(self.rotRad),
                 ],
                 [
-                    sqrt(sin(self.rotRad)),
-                    sqrt(cos(self.rotRad)),
+                    (sin(self.rotRad)) ** 2,
+                    (cos(self.rotRad)) ** 2,
                     -2 * sin(self.rotRad) * cos(self.rotRad),
                 ],
                 [
                     -sin(self.rotRad) * cos(self.rotRad),
                     sin(self.rotRad) * cos(self.rotRad),
-                    sqrt(cos(self.rotRad)) - sqrt(sin(self.rotRad)),
+                    (cos(self.rotRad)) ** 2 - (sin(self.rotRad)) ** 2,
                 ],
             ]
         )
@@ -309,19 +310,19 @@ class Ply:
         self.rotElongation = np.array(
             [
                 [
-                    sqrt(cos(self.rotRad)),
-                    sqrt(sin(self.rotRad)),
+                    (cos(self.rotRad)) ** 2,
+                    (sin(self.rotRad)) ** 2,
                     sin(self.rotRad) * cos(self.rotRad),
                 ],
                 [
-                    sqrt(sin(self.rotRad)),
-                    sqrt(cos(self.rotRad)),
+                    (sin(self.rotRad)) ** 2,
+                    (cos(self.rotRad)) ** 2,
                     -sin(self.rotRad) * cos(self.rotRad),
                 ],
                 [
                     -2 * sin(self.rotRad) * cos(self.rotRad),
                     2 * sin(self.rotRad) * cos(self.rotRad),
-                    sqrt(cos(self.rotRad)) - sqrt(sin(self.rotRad)),
+                    (cos(self.rotRad)) ** 2 - (sin(self.rotRad)) ** 2,
                 ],
             ]
         )
@@ -337,6 +338,12 @@ class Ply:
             self.rotStress,
             np.matmul(self.mat.stiffnessMatrix, np.linalg.inv(self.rotElongation)),
         )
+
+    def calc_engineer_constantes(self):
+        self.E_1 = 1 / self.S[0, 0]
+        self.E_2 = 1 / self.S[1, 1]
+        self.G = 1 / self.S[2, 2]
+        self.v_12 = -self.E_1 * self.S[0, 1]
 
 
 class Laminate:

tests/test_clt_py.py
@@ -8,6 +8,12 @@ import pytest
 from clt_py import *
 from clt_py.clt_py import *
 
+from math import log10, floor
+
+
+def round_sig(x, sig=2):
+    return round(x, sig - int(floor(log10(abs(x)))))
+
 
 def test_IsotropicMaterial_NotEnoughArgumentError():
     with pytest.raises(NotEnoughArgumentError):
@@ -160,6 +166,8 @@ def test_Ply_rotationStressMatrix():
     assert round(ply0.Q[2, 2], 5) == round(ply90.Q[2, 2], 5)
     assert round(ply0.Q[0, 1], 5) == round(ply90.Q[0, 1], 5)
     assert round(ply0.Q[1, 0], 5) == round(ply90.Q[1, 0], 5)
+    assert round(ply0.Q[1, 2], 5) == 0
+    assert round(ply0.Q[0, 2], 5) == 0
 
     assert round(ply0.S[0, 0], 5) == round(ply90.S[1, 1], 5)
     assert round(ply0.S[1, 1], 5) == round(ply90.S[0, 0], 5)
@@ -180,10 +188,10 @@ def test_Ply_rotationStressMatrix():
     assert round(ply45.Q[2, 2], 5) == round(
         (0.25 * (ply0.Q[0, 0] + ply0.Q[1, 1]) - 0.5 * ply0.Q[0, 1]), 5
     )
-    assert round(ply45.Q[0, 2], 5) == 0
-    assert round(ply45.Q[1, 2], 5) == 0
-    assert round(ply45.Q[2, 0], 5) == 0
-    assert round(ply45.Q[2, 1], 5) == 0
+    assert round(ply45.Q[1, 2], 5) == round(0.25 * (ply0.Q[1, 1] - ply0.Q[0, 0]), 5)
+    assert round(ply45.Q[0, 2], 5) == round(0.25 * (ply0.Q[1, 1] - ply0.Q[0, 0]), 5)
+    assert round(ply45.Q[2, 0], 5) == round(0.25 * (ply0.Q[1, 1] - ply0.Q[0, 0]), 5)
+    assert round(ply45.Q[2, 1], 5) == round(0.25 * (ply0.Q[1, 1] - ply0.Q[0, 0]), 5)
 
     assert round(ply45.S[0, 0], 5) == round(
         (
@@ -207,10 +215,49 @@ def test_Ply_rotationStressMatrix():
     assert round(ply45.S[2, 2], 5) == round(
         ((ply0.S[0, 0] + ply0.S[1, 1]) - 2 * ply0.S[0, 1]), 5
     )
-    assert round(ply45.S[0, 2], 5) == 0
-    assert round(ply45.S[1, 2], 5) == 0
-    assert round(ply45.S[2, 0], 5) == 0
-    assert round(ply45.S[2, 1], 5) == 0
+    assert round(ply45.S[0, 2], 5) == round(0.5 * (ply0.S[1, 1] - ply0.S[0, 0]), 5)
+    assert round(ply45.S[1, 2], 5) == round(0.5 * (ply0.S[1, 1] - ply0.S[0, 0]), 5)
+    assert round(ply45.S[2, 0], 5) == round(0.5 * (ply0.S[1, 1] - ply0.S[0, 0]), 5)
+    assert round(ply45.S[2, 1], 5) == round(0.5 * (ply0.S[1, 1] - ply0.S[0, 0]), 5)
+
+
+def test_Ply_real_problem():
+    # Carbon fiber: T300
+    fiber_rho = 1.74e3
+    fiber_E_para = 2.2e5
+    fiber_E_ortho = 2.8e4
+    fiber_G = 5e4
+    fiber_v_para_ortho = 2.3e-1
+
+    # Epoxyd matrix:
+    matrix_rho = 1.32e3
+    matrix_E = 3.65e3
+    matrix_v = 3e-1
+
+    mat_Epoxy = IsotropicMaterial(matrix_rho, E=matrix_E, v=matrix_v, label="Epoxy")
+    mat_CFiber = AnisotropicMaterial(
+        fiber_rho,
+        fiber_E_para,
+        fiber_E_ortho,
+        fiber_G,
+        fiber_v_para_ortho,
+        label="C-Fiber T300",
+    )
+
+    FiberReinforcedMaterialUD.set_system("prismatic_jones")
+
+    crp = FiberReinforcedMaterialUD(mat_CFiber, mat_Epoxy, label="CRP")
+    crp.set_fibWgRatio(0.665)
+
+    assert round_sig(crp.E_para, sig=3) == 1.337e5
+    assert round_sig(crp.E_ortho, sig=3) == 7.646e3
+    assert round_sig(crp.G, sig=3) == 3.375e3
+    assert round_sig(crp.v_para_ortho, sig=2) == 2.58e-1
+    assert round_sig(crp.v_ortho_para, sig=2) == 1.48e-2
+
+    ply = Ply(crp, rotation=45)
+    assert round_sig(ply.G, sig=3) == 7.036e3
+    assert round_sig(ply.E_1, sig=3) == 9.287e3
 
 
 def test_Laminate_add():
@@ -329,38 +376,6 @@ def test_Laminate_stiffnessMatrix_orthotrop_as_disc():
     assert np.array_equal(lam_matrix, compare_matrix)
 
 
-def test_Laminate_stiffnessMatrix_orthotrop_as_plate():
-    matMat = IsotropicMaterial(rho=1, E=1, v=0.25)
-    matFib = AnisotropicMaterial(rho=2, v_para_ortho=0.25, E_para=10, E_ortho=2, G=3)
-
-    mat_FRM = FiberReinforcedMaterialUD(matFib=matFib, matMat=matMat)
-
-    plyP45 = Ply(mat_FRM, rotation=45, thickness=1)
-    plyN45 = Ply(mat_FRM, rotation=-45, thickness=6)
-
-    laminate = Laminate()
-    laminate.addPly(plyP45)
-    laminate.addPly(plyN45)
-    laminate.addPly(plyP45)
-
-    # assert preprocessing
-    np.set_printoptions(precision=3, suppress=True)
-    print(laminate.get_stiffnessMatrix())
-
-    compare_matrix = np.zeros((6, 6))
-    compare_matrix[0:3, 0:3] = 1
-    compare_matrix[3:5, 3:5] = 1
-    compare_matrix[5:6, 5:6] = 1
-    print(compare_matrix)
-
-    (bx, by) = np.nonzero(np.round(laminate.get_stiffnessMatrix(), 3))
-    lam_matrix = np.zeros(laminate.get_stiffnessMatrix().shape)
-    for x, y in zip(bx, by):
-        lam_matrix[x, y] = 1
-
-    assert np.array_equal(lam_matrix, compare_matrix)
-
-
 def test_Laminate_stiffnessMatrix_exzentric_orthotrop():
     matMat = IsotropicMaterial(rho=1, E=1, v=0.25)
     matFib = AnisotropicMaterial(rho=2, v_para_ortho=0.25, E_para=10, E_ortho=2, G=3)
@@ -368,12 +383,14 @@ def test_Laminate_stiffnessMatrix_exzentric_orthotrop():
     mat_FRM = FiberReinforcedMaterialUD(matFib=matFib, matMat=matMat)
 
     ply0 = Ply(mat_FRM, rotation=0, thickness=1)
-    ply90 = Ply(mat_FRM, rotation=90, thickness=3)
+    ply90 = Ply(mat_FRM, rotation=90, thickness=2)
 
     laminate = Laminate()
     laminate.addPly(ply0)
     laminate.addPly(ply90)
 
+    print(ply0.Q)
+    print(ply90.Q)
     # assert preprocessing
     np.set_printoptions(precision=3, suppress=True)
     print(laminate.get_stiffnessMatrix())